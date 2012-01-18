c411c2af2d3355f6e1bd178c2451a7678ec94211
Wainui <mr.snow+wainui@houseoflaudanum.com>
2012-01-18 13:38:23 +1100

    #36 better path for git pull

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -1,6 +1,13 @@
 <?php
+  $source_dir = "/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/";
 // Use in the "Post-Receive URLs" section of your GitHub repo.
-  if ( $_POST['payload'] ) {
-    shell_exec( 'cd /home/laudanum/public_html/blackbeetle/blackbeetle_profile/ && git reset --hard HEAD && git pull' );
+  if ( $_REQUEST['payload'] ) {
+    echo shell_exec('whoami');
+    echo "<br />"; 
+    echo shell_exec('cd ' . $source_dir . '  && git reset --hard HEAD && git pull');
+    echo "<br />"; 
+    print_r($_REQUEST['payload']);
+    echo "<br />"; 
   }
+	print "OK:";
 ?>
