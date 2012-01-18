0f7b8cb6777d9bd8812953f09802bf56a3516826
Alex Li <firstplayerzerone@gmail.com>
2012-01-18 09:37:59 +0800

    issue #32
    I have removed static blocks regardless footer regions on template

--- themes/custom/blackbeetle/templates/page--page.tpl.php
+++ themes/custom/blackbeetle/templates/page--page.tpl.php
@@ -30,15 +30,10 @@
             </div>
             <div id="footer">
                 <div class="footer_body clearfix">
-                    <div class="contacts">
-                        <div class="phone">ph: +61 2 9999 5555</div>
-                    </div>
                     <?php print render($page['footer_left']); ?>
-                    
-                    <div class="site-info"> <?php print $site_slogan; ?></div>
                     <?php print render($page['footer_right']); ?>
                 </div>
-                <div id="site-name" class="footer-right">
+                <div id="site-name">
                     <h1><?php print $site_name; ?></h1>
                 </div>
             </div>
