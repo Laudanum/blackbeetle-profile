ec51cde1aa0fa201484fc036a3023ede51baaffa
Mr Snow <mr.snow@houseoflaudanum.com>
2012-01-18 14:40:38 +1100

    #36 git reset again after pull

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -2,7 +2,8 @@
   $REPO = "/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git";
 // Use in the "Post-Receive URLs" section of your GitHub repo.
   if ( $_REQUEST['payload'] ) {
-    $command = ("git --git-dir=$REPO reset --hard HEAD && git --git-dir=$REPO pull");
+//  not sure why we have to reset afterwards too but otherwise the changes don't come in
+    $command = ("git --git-dir=$REPO reset --hard HEAD && git --git-dir=$REPO pull && git --git-dir=$REPO reset --hard HEAD");
     echo shell_exec($command);
   }
   print "\nOK";
