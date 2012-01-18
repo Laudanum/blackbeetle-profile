3d70dc4942d5cdc23e5a56336bf2190ad4fb0158
Wainui <mr.snow+wainui@houseoflaudanum.com>
2012-01-18 13:52:43 +1100

    #36 www-data doesn't have cd

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -6,7 +6,7 @@
     echo "<br />";
     echo shell_exec('ls -la '. $source_dir); 
     echo "<br />";
-    echo shell_exec('cd ' . $source_dir . ' && git reset --hard HEAD && git pull');
+    echo shell_exec('GIT_DIR' . $source_dir . ' && git reset --hard HEAD && git pull');
     echo "<br />"; 
     print_r($_REQUEST['payload']);
     echo "<br />"; 
