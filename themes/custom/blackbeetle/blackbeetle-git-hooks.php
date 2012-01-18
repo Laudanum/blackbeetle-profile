f8c94783bab9d068df0e7652027ee1a4268e7f43
Mr Snow <mr.snow@houseoflaudanum.com>
2012-01-18 14:49:47 +1100

    #36 git reset again after pull with semicolon post reset

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -1,4 +1,30 @@
 <?php
+/*
+# discover the ssh key directories used for home by www-data
+sudo -u www-data ssh -v git@github.com
+debug1: identity file /var/www/.ssh/id_rsa type -1
+
+# make a keey for the www-data user
+sudo -u www-data mkdir /var/www/.ssh/; sudo -u www-data ssh-keygen -f /var/www/.ssh/id_rsa && sudo -u www-data less /var/www/.ssh/id_rsa.pub
+
+# add the key to github
+
+# target our repo - note the trailing .git
+REPO=/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git
+
+# chown everything in the repo to www-data
+sudo chown -R www-data $REPO
+
+# test the method
+sudo -u www-data git --git-dir=$REPO reset --hard HEAD && sudo -u www-data git --git-dir=$REPO pull
+
+# create a php file to hit from github
+sudo -u www-data git --git-dir=/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git reset --hard HEAD && sudo -u www-data git --git-dir=/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git pull
+
+# point github to it
+admin > Repository Administration > Service Hooks > Post-Receive URLs
+*/
+
   $REPO = "/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git";
 // Use in the "Post-Receive URLs" section of your GitHub repo.
   if ( $_REQUEST['payload'] ) {
