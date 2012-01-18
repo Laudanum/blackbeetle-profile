f046660b3081a459eaf1f70f2ac03a8f220a67be
Wainui <mr.snow+wainui@houseoflaudanum.com>
2012-01-18 14:22:58 +1100

    #36 set up for www-data user

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -1,12 +1,12 @@
 <?php
-  $source_dir = "/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/";
+  $REPO = "/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git";
 // Use in the "Post-Receive URLs" section of your GitHub repo.
   if ( $_REQUEST['payload'] ) {
     echo shell_exec('whoami');
     echo "<br />";
-    echo shell_exec('ls -la '. $source_dir); 
+    echo shell_exec('ls -la '. $REPO); 
     echo "<br />";
-    echo shell_exec('GIT_DIR' . $source_dir . ' && git reset --hard HEAD && git pull');
+    echo shell_exec("sudo -u www-data git --git-dir=$REPO reset --hard HEAD && sudo -u www-data git --git-dir=$REPO pull");
     echo "<br />"; 
     print_r($_REQUEST['payload']);
     echo "<br />"; 
