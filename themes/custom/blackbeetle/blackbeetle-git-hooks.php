f3c71c66b38d6f77b3ea000e63008baf08f67184
Mr Snow <mr.snow@houseoflaudanum.com>
2012-01-18 14:42:47 +1100

    #36 git reset again after pull with semicolon

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -3,7 +3,7 @@
 // Use in the "Post-Receive URLs" section of your GitHub repo.
   if ( $_REQUEST['payload'] ) {
 //  not sure why we have to reset afterwards too but otherwise the changes don't come in
-    $command = ("git --git-dir=$REPO reset --hard HEAD && git --git-dir=$REPO pull && git --git-dir=$REPO reset --hard HEAD");
+    $command = ("git --git-dir=$REPO reset --hard HEAD && git --git-dir=$REPO pull; git --git-dir=$REPO reset --hard HEAD");
     echo shell_exec($command);
   }
   print "\nOK";
