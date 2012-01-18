25c0f2b1d684239071f7ed8feadb6386873d355b
Wainui <mr.snow+wainui@houseoflaudanum.com>
2012-01-18 14:29:17 +1100

    #36 best guess for git push hook

--- themes/custom/blackbeetle/blackbeetle-git-hooks.php
+++ themes/custom/blackbeetle/blackbeetle-git-hooks.php
@@ -2,14 +2,8 @@
   $REPO = "/home/laudanum/public_html/blackbeetle.com.au/blackbeetle-profile/.git";
 // Use in the "Post-Receive URLs" section of your GitHub repo.
   if ( $_REQUEST['payload'] ) {
-    echo shell_exec('whoami');
-    echo "<br />";
-    echo shell_exec('ls -la '. $REPO); 
-    echo "<br />";
-    echo shell_exec("sudo -u www-data git --git-dir=$REPO reset --hard HEAD && sudo -u www-data git --git-dir=$REPO pull");
-    echo "<br />"; 
-    print_r($_REQUEST['payload']);
-    echo "<br />"; 
+    $command = ("git --git-dir=$REPO reset --hard HEAD && git --git-dir=$REPO pull");
+    echo shell_exec($command);
   }
-	print "OK:";
+  print "\nOK";
 ?>
